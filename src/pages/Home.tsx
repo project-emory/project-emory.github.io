03321fe0b26ea33e5989498e922c647840c0643d
andrewtlu <andrew.lu@emory.edu>
2024-07-30 20:21:28 -0400

    Add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Project Emory</h1>
+            <p>How are you doing today?</p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, possimus. Eos ipsa labore nihil blanditiis asperiores aspernatur autem ut esse atque aliquid accusamus quo fugiat, dignissimos laboriosam voluptatum ratione fugit?
+            </p>
+        </div>
+    );
+}
