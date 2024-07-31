09560ba6f59c61053fe9e75690f6597135704af9
andrewtlu <andrew.lu@emory.edu>
2024-07-30 20:53:55 -0400

    Add common styling, "pages" sections (single page for now)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import "./Home.css";
+import AboutUs from "./AboutUs";
+import Upcoming from "./Upcoming";
+import Contact from "./Contact";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Project Emory</h1>
-            <p>How are you doing today?</p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, possimus. Eos ipsa
-                labore nihil blanditiis asperiores aspernatur autem ut esse atque aliquid accusamus
-                quo fugiat, dignissimos laboriosam voluptatum ratione fugit?
-            </p>
-        </div>
+        <>
+            <div>
+                <h1>Project Emory</h1>
+                <p>How are you doing today?</p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, possimus. Eos
+                    ipsa labore nihil blanditiis asperiores aspernatur autem ut esse atque aliquid
+                    accusamus quo fugiat, dignissimos laboriosam voluptatum ratione fugit?
+                </p>
+            </div>
+
+            <AboutUs />
+
+            <Upcoming />
+
+            <Contact />
+        </>
     );
 }
