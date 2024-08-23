04f8c65994b96b5ac4ec94e46c6abc74fe709ce4
andrewtlu <andrew.lu@emory.edu>
2024-08-23 16:53:20 -0400

    Add intro hero animation

--- src/pages/hero/Hero.tsx
+++ src/pages/hero/Hero.tsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import "./Hero.css";
 import { Logo } from "components/Logo";
 
 export default function Hero() {
+    const [loaded, setLoaded] = useState(true);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, []);
+
     return (
         <div className="hero">
-            <div className="hero-element-l">
-                {/* this is incredibly goofy & jank but works for now */}
-                <Logo
-                    background="var(--pe-bg)"
-                    longArcColor="var(--pe-blue)"
-                    shortArcColor="var(--pe-yellow)"
-                />
+            <div className={`hero-element-l ${loaded ? "tilted centered-l" : ""}`}>
+                <a href="http://github.com/project-emory" target="_blank" rel="noopener noreferrer">
+                    {/* this is incredibly goofy & jank but works for now */}
+                    <Logo
+                        background="var(--bg-base)"
+                        longArcColor="var(--pe-blue-muted)"
+                        shortArcColor="var(--pe-yellow-muted)"
+                    />
+                </a>
             </div>
-            <div className="hero-element-r">
+            <div className={`hero-element-r ${loaded ? "centered-r" : ""}`}>
                 <h1>Project Emory</h1>
                 <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, possimus. Eos
