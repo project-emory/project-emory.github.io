212619c12961f566680489e505e7e03a262d2233
andrewtlu <andrew.lu@emory.edu>
2024-08-27 11:41:51 -0400

    Svelte + Sveltekit time hehe

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            components: "/src/components",
-            pages: "/src/pages",
-            "@": "/src",
-        },
-    },
+    plugins: [sveltekit()]
 });
