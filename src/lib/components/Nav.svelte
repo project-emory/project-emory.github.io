0fabd18924580cb15be083b8ca416748db6dbcd5
andrewtlu <andrewlulu2012@gmail.com>
2026-04-27 19:58:04 -0400

    Add daisyui

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,2 @@
+<div class="toast toast-start toast-top">logo</div>
+<div class="toast toast-end toast-top">hamburger</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import "./layout.css";
     import favicon from "$lib/assets/favicon.svg";
+    import Nav from "$lib/components/Nav.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 
     let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Nav />
+<Footer />
 {@render children()}
