d5f963da100cea41c672a3fcf5c452f49743f495
andrewtlu <andrewlulu2012@gmail.com>
2026-04-29 12:06:45 -0400

    Fix carousel pixel offsets

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -4,10 +4,16 @@
     import { events, projects } from "$lib/data/showcase";
 </script>
 
+<!--
+note to future maintainers: padding on carousel can cause pixel offsets if one
+carousel is full and the other is not, but shadow will not show since overflow
+is hidden
+-->
+
 <div class="flex flex-col gap-4">
     <div class="flex flex-row gap-4">
         <div class="text-title [writing-mode:vertical-rl]">projects</div>
-        <div class="carousel gap-4 p-1">
+        <div class="carousel w-full carousel-start gap-4">
             {#each projects as project (project.slug)}
                 <Tile
                     img={project.images.length > 0 ? project.images[0] : logo}
@@ -19,7 +25,7 @@
     </div>
     <div class="flex flex-row gap-4">
         <div class="text-title [writing-mode:vertical-rl]">events</div>
-        <div class="carousel gap-4 p-1">
+        <div class="carousel w-full carousel-start gap-4">
             {#each events as event (event.slug)}
                 <Tile
                     img={event.images.length > 0 ? event.images[0] : logo}
